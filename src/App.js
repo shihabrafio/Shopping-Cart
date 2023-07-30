8a5d251454d5ed29168d8c7dbabacc74b44f235e
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-30 12:23:40 +0600

    Update App files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
+import { Header } from "./components";
+import { Allroutes } from "./routes/Allroutes";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Allroutes/>
       <Footer/>
     </div>
   )
