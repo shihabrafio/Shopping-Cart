f2eeaa680adc9f8aa66d478c021ba1dcd19ed3fd
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-30 12:26:50 +0600

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white shadow dark:bg-gray-800">
+    <footer className="bg-white shadow ">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="text-sm text-gray-500 sm:text-center">
           © 2023{" "}
           <Link to="/" className="hover:underline">
-            Sky Movie™
+            Shopping Cart™
           </Link>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500">
           <li>
             <a href="https://github.com/shihabrafio" className="mr-4 hover:underline md:mr-6 ">
               Github
